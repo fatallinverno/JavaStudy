51f57815742ce317043f62937547e7393f83845d
Linverno <fatal_linverno@naver.com>
2024-04-15 16:42:26 +0900

    정수 타입

--- src/ch02/sec02/IntegerLiteral.java
+++ src/ch02/sec02/IntegerLiteral.java
@@ -13,7 +13,9 @@ package ch02.sec02;
  * */
 
 public class IntegerLiteral {
+
     public static void main(String[] args) {
+
         int var1 = 0b1011;     //2진수
         int var2 = 0206;       //8진수
         int var3 = 365;        //10진수
@@ -23,5 +25,7 @@ public class IntegerLiteral {
         System.out.println("var2 = " + var2);
         System.out.println("var3 = " + var3);
         System.out.println("var4 = " + var4);
+
     }
+
 }
