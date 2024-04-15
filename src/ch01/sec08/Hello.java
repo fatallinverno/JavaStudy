c6e076e40ba1d98e3b81e8cffe8706797f054fec
Linverno <fatal_linverno@naver.com>
2024-04-15 16:42:17 +0900

    자바 시작

--- src/ch01/sec08/Hello.java
+++ src/ch01/sec08/Hello.java
@@ -1,15 +1,18 @@
 package ch01.sec08;
 
 /*
-* 장제목 : 1장 자바 시작하기
+* 장제목 : 1 자바 시작하기
 * 작성일 : 2024.04.15
 * */
 
 public class Hello { //Hello 클래스명 = 소스파일명
+
     //프로그램 진입점
     public static void main(String[] args) { // main 메소드명
+
         //콘솔에 출력하는 실행문
         System.out.println("Hello, Java");
 //        System.out.println("Hello, /*주석이 될 수 없음*/ Java");
     }
+
 }
