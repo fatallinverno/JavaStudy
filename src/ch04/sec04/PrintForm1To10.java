99dde5d7fa4f2ecca0c2c4273790514d3a7de322
Linverno <fatal_linverno@naver.com>
2024-04-19 14:14:30 +0900

    for 문

---
+++ src/ch04/sec04/PrintForm1To10.java
@@ -0,0 +1,15 @@
+package ch04.sec04;
+
+/*
+ * 장제목 : 4-4 for 문
+ * 작석일 : 2024.04.19
+ *
+ * */
+
+public class PrintForm1To10 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
