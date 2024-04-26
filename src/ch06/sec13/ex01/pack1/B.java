3c5bf1d77c7261431d9d9f5360ae0c59ae92224a
Linverno <fatal_linverno@naver.com>
2024-04-26 10:09:01 +0900

    접근 제한자

---
+++ src/ch06/sec13/ex01/pack1/B.java
@@ -0,0 +1,6 @@
+package ch06.sec13.ex01.pack1;
+
+public class B {
+    //필드 선언
+    A a;    //O <--- A 클래스 접근 가능(필드로 선언할 수 있음)
+}
---
+++ src/ch06/sec13/ex01/pack2/C.java
@@ -0,0 +1,10 @@
+package ch06.sec13.ex01.pack2;
+
+import ch06.sec13.ex01.pack1.*;
+
+public class C {
+    //필드 선언
+//    A a;    //X <--- A 클래스 접근 불가(컴파일 에러)
+    B b;    //O
+
+}
