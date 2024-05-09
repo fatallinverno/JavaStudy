08e8888ff75b6774e39fbe547d1ceafc00d75244
Linverno <fatal_linverno@naver.com>
2024-05-09 17:06:21 +0900

    문구 변경

--- src/ch07/sec10/ex01/SmartPhoneEx.java
+++ src/ch07/sec10/ex01/SmartPhoneEx.java
@@ -2,7 +2,7 @@ package ch07.sec10.ex01;
 
 public class SmartPhoneEx {
     public static void main(String[] args) {
-        //Phone phone = new Phone(); //사용 불가
+        //Phone phone = new Phone(); //사용X
 
         SmartPhone smartPhone = new SmartPhone("홍길동");
 
