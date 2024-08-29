f37c42630c320aa3cd0805e2f7742095c27c62d1
Linverno <fatal_linverno@naver.com>
2024-08-29 14:20:22 +0900

    스레드 동기화 - 동기화 메소드 및 블록선언

--- src/ch14/sec06/ex01/SynchronizedEx.java
+++ src/ch14/sec06/ex01/SynchronizedEx.java
@@ -7,6 +7,7 @@ public class SynchronizedEx {
         User1Thread user1Thread = new User1Thread();
         user1Thread.setCalculator(calculator);
         user1Thread.start();
+
         User2Thread user2Thread = new User2Thread();
         user2Thread.setCalculator(calculator);
         user2Thread.start();
