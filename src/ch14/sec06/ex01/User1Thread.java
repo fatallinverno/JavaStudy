a464e99c1a7b0c57e85a36a08026d562843652b7
Linverno <fatal_linverno@naver.com>
2024-08-29 14:20:34 +0900

    스레드 동기화 - 동기화 메소드 및 블록선언

--- src/ch14/sec06/ex01/User1Thread.java
+++ src/ch14/sec06/ex01/User1Thread.java
@@ -10,7 +10,6 @@ public class User1Thread extends Thread {
     public void setCalculator(Calculator calculator) {
         this.calculator = calculator;
     }
-
     @Override
     public void run() {
         calculator.setMemory1(100);
